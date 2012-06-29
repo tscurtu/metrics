8247c5da804117e80d744d5016e75ee89b2d6a65
Tudor Scurtu <tscurtu@adobe.com>
2012-06-29 10:48:59 +0300

    Implemented comments.

--- metrics-core/src/main/java/com/yammer/metrics/core/RollingRecord.java
+++ metrics-core/src/main/java/com/yammer/metrics/core/RollingRecord.java
@@ -6,32 +6,32 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A counter that records the absolute number of events that have occurred in the last
- * specified time interval.
+ * specified time buckets.
  */
 public class RollingRecord extends Counter {
     private LinkedList<Long> eventBuckets;
     private AtomicLong total;
-    private long interval;
+    private long buckets;
 
     /**
      * Creates a new {@link RollingRecord}.
      *
-     * @param interval    the interval for which this counter will record events
-     * @param timeUnit    the time unit of the forementioned interval
-     * @param secondsTick the update interval in seconds
+     * @param buckets    the buckets for which this counter will record events
+     * @param timeUnit    the time unit of the forementioned buckets
+     * @param secondsTick the update buckets in seconds
      */
-    RollingRecord(long interval, TimeUnit timeUnit, long secondsTick) {
+    RollingRecord(long buckets, TimeUnit timeUnit, long secondsTick) {
         this.eventBuckets = new LinkedList<Long>();
         this.total = new AtomicLong(0L);
-        this.interval = timeUnit.toMillis(interval) / secondsTick / 1000L;
+        this.buckets = timeUnit.toMillis(buckets) / secondsTick / 1000L;
 
-        if (this.interval <= 0) {
-            throw new IllegalArgumentException("Invalid interval-tick combination");
+        if (this.buckets <= 0) {
+            throw new IllegalArgumentException("Invalid buckets-tick combination");
         }
     }
 
     public synchronized void tick() {
-        if (eventBuckets.size() + 1 >= interval) {
+        if (eventBuckets.size() + 1 >= buckets) {
             total.addAndGet(-eventBuckets.removeLast());
         }
         eventBuckets.addFirst(count.getAndSet(0L));
